9b7d5765a45a24c782a816fca54b62747577a407
Davidson Raj <davidson.raj@opendoor.com>
2021-11-06 13:06:45 -0400

    initial commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,15 +6,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'Bible',
+  tagline: '',
+  url: 'https://davidson-r.github.io/learningdocusaurus',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'davidson-r', // Usually your GitHub org/user name.
+  projectName: 'learningdocusaurus', // Usually your repo name.
 
   presets: [
     [
